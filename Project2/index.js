4cd7c3287b519c110d572f34c7419811c17090ee
zahinAlMahmud <167842947+zahinAlMahmud@users.noreply.github.com>
2025-08-05 12:18:41 -0400

    Create index.js

---
+++ Project2/index.js
@@ -0,0 +1,21 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle('active', i === index);
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+}
+
+// Show the first slide on load
+showSlide(currentSlide);
